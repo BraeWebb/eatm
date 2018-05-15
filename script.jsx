1a7064e6d9e5d4fa7f4a114b9565f309971d94b7
m-crofts <30452973+m-crofts@users.noreply.github.com>
2018-05-15 18:57:02 +1000

    Add new screens

--- script.jsx
+++ script.jsx
@@ -1,41 +1,138 @@
-class ScreenOne extends React.Component {
+/*
+====================
+Screens
+====================
+*/
+
+class LandingScreen extends React.Component {
   constructor(props) {
     super(props);
     this.handleContinue = this.handleContinue.bind(this);
   }
 
   handleContinue() {
-    this.props.onContinue(1);
+    this.props.onContinue('card');
   }
 
   render() {
     return (
       <div onClick={this.handleContinue}>
-        <h1>1</h1>
-        <p>{this.props.input}</p>
-        <p>Touch screen to continue</p>
+        <h1>Welcome to BankX</h1>
+        <p>Touch screen to begin</p>
       </div>
     );
   }
 }
 
-class ScreenTwo extends React.Component {
+class ScanCardScreen extends React.Component {
   constructor(props) {
     super(props);
     this.handleContinue = this.handleContinue.bind(this);
   }
 
   handleContinue() {
-    this.props.onContinue(0);
+    this.props.onContinue('pin');
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleContinue}>
+        <p>Please place your card on the reader</p>
+      </div>
+    );
+  }
+}
+
+class PINScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
+    // this is probably unsafe...
+    // but it seems to work without either an error or a warning
+    if (nextProps.ok == true) {
+      this.props.onContinue('home');
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>2</h1>
-        <p>{this.props.input}</p>
-        <button onClick={this.handleContinue}>Next</button>
+        <p>Please enter your PIN</p>
+        <input type="password" value={this.props.input} readOnly="true" />
       </div>
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleContinue = this.handleContinue.bind(this);
+  }
+
+  handleContinue() {
+    this.props.onContinue('withdrawAccount');
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleContinue}>
+        <p>HOME SCREEN PLACEHOLDER</p>
+      </div>
+    );
+  }
+}
+
+class OptionScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(index, e) {
+    this.props.options[index].callback();
+    this.props.onContinue(this.props.continueTo);
+  }
+
+  render() {
+    const optionRows = this.props.options.map((option, index) =>
+      <div key={index} className="col-xs-12">
+        <div className="button" onClick={(e) => this.handleClick(index, e)}>{option.name}</div>
+      </div>
+    );
+    return (
+      <div>
+        <p>{this.props.prompt}</p>
+        <div className="row around-xs">
+          {optionRows}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+/*
+====================
+ATM
+====================
+*/
+
+class Breadcrumbs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.onClick(this.props.keyValue);
+  }
+
+  render() {
+    return (
+      <nav className="breadcrumbs" onClick={this.handleClick}></nav>
     );
   }
 }
@@ -66,7 +163,29 @@ class ATM extends React.Component {
     this.pushScreen = this.pushScreen.bind(this);
     this.popScreen = this.popScreen.bind(this);
     this.goBack = this.goBack.bind(this);
-    this.state = {input: '', screen: 0, crumbs: [0], account: '', amount: '', favourites: []};
+    this.setOk = this.setOk.bind(this);
+    this.setAccountToSavings = this.setAccountToSavings.bind(this);
+    this.setAccountToCheque = this.setAccountToCheque.bind(this);
+    this.setAccountToCredit = this.setAccountToCredit.bind(this);
+
+    // i think this is all we need to keep track of
+    this.state = {
+      input: '',
+      screen: 'landing',
+      crumbs: [0],
+      ok: false,
+      card: false,
+      amount: 0,
+      accountType: '',
+      depositType: '',
+      chequeIn: false,
+      cashIn: false,
+      cashOut: false,
+      chequeInLight: false,
+      cashInLight: false,
+      cashOutLight: false,
+      favourites: []
+    };
   }
 
   handleInput(value) {
@@ -77,8 +196,16 @@ class ATM extends React.Component {
     this.setState({input: ''});
   }
 
-  pushScreen(i) {
-    this.setState({screen: i, crumbs: this.state.crumbs.concat([i])});
+  pushScreen(key) {
+    this.setState({
+      screen: key,
+      crumbs: this.state.crumbs.concat([key]),
+      ok: false,
+      card: false,
+      chequeIn: false,
+      cardIn: false,
+      cashOut: false
+    });
   }
 
   popScreen(n) {
@@ -89,15 +216,47 @@ class ATM extends React.Component {
     this.setState({screen: crumbs[crumbs.length - 1], crumbs: crumbs});
   }
 
-  goBack() {
+  goBack(value) {
     this.popScreen(1);
   }
 
+  setOk(value) {
+    this.setState({ok: true});
+  }
+
+  setAccountToSavings() {
+    this.setState({account: 'Savings'});
+  }
+
+  setAccountToCheque() {
+    this.setState({account: 'Cheque'});
+  }
+
+  setAccountToCredit() {
+    this.setState({account: 'Credit'});
+  }
+
   render() {
-    const screens = [
-      <ScreenOne input={this.state.input} onContinue={this.pushScreen} />,
-      <ScreenTwo input={this.state.input} onContinue={this.pushScreen} />
+    const withdrawAccountOptions = [
+      {name: 'Savings', callback: this.setAccountToSavings},
+      {name: 'Cheque', callback: this.setAccountToCheque},
+      {name: 'Credit', callback: this.setAccountToCredit}
     ];
+    // the array keys are used to identify screens
+    const screens = {
+      /*
+      -1: Error
+      */
+      landing: <LandingScreen onContinue={this.pushScreen} />,
+      card: <ScanCardScreen input={this.state.input} onContinue={this.pushScreen} />,
+      pin: <PINScreen input={this.state.input} ok={this.state.ok} onContinue={this.pushScreen} />,
+      home: <HomeScreen onContinue={this.pushScreen} />,
+      withdrawAccount: <OptionScreen prompt="Choose an account" options={withdrawAccountOptions} onContinue={this.pushScreen} continueTo="landing" />
+      /*
+      98: Transaction pending
+      99: Transaction complete
+      */
+    };
 
     return (
       <div className="row middle-xs">
@@ -107,6 +266,7 @@ class ATM extends React.Component {
               <div className="col-xs-8">
                 <h1 className="bank">BankX</h1>
                 <div className="screen">
+                  <Breadcrumbs />
                   {screens[this.state.screen]}
                 </div>
               </div>
@@ -136,7 +296,7 @@ class ATM extends React.Component {
                     <Key keyValue="" />
                     <Key keyValue="0" onClick={this.handleInput} />
                     <Key keyValue="." onClick={this.handleInput} />
-                    <Key keyValue="OK" />
+                    <Key keyValue="OK" onClick={this.setOk} />
                   </div>
                 </div>
               </div>
