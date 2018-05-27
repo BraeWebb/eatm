e8ea000518766557fb602b5d52550a348099f262
BraeWebb <email@braewebb.com>
2018-05-27 11:52:58 +1000

    Server for session storage

---
+++ server/main.js
@@ -0,0 +1,57 @@
+let MongoClient = require('mongodb').MongoClient;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+let db;
+
+MongoClient.connect('mongodb://eatm:trevorsucks@eatm.braewebb.com:27017/eatm', function (err, client) {
+  if (err) throw err;
+
+  db = client.db('eatm');
+
+  app.listen(8081);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.get('/sessions', function (req, res) {
+  db.collection('sessions').find().toArray(function (err, result) {
+    if (err) throw err;
+
+    res.send(result);
+  })
+});
+
+app.post('/session', function (req, res) {
+  db.collection("sessions").insertOne({
+    history: req.body.history,
+    name: req.body.name,
+    submitted: new Date()
+  }, function(err, res) {
+    if (err) throw err;
+  });
+});
+
+app.get('/reset', function (req, res) {
+  db.collection("sessions").drop(function(err, delOK) {
+    if (err) throw err;
+    if (delOK) console.log("Collection deleted");
+  });
+
+  db.createCollection("sessions", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+  });
+});
+
+
