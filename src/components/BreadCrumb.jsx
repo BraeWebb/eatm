f23dd5872b00bb013f3e35e660173f3b339dab05
BraeWebb <email@braewebb.com>
2018-05-27 16:48:21 +1000

    Improved Breadcrumbs

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -4,6 +4,16 @@ class BreadCrumb extends React.Component {
   constructor(props) {
     super(props);
     this.crumbClick = this.crumbClick.bind(this);
+    this.labels = {
+      home: "Home",
+      help: "Help",
+      error: "Error",
+      withdrawal: "Amount",
+      withdrawalAccount: "Account",
+      withdrawalConfirmation: "Confirm",
+      withdrawalFavourite: "Favourite",
+      withdrawalCustom: "Custom"
+    };
   }
 
   crumbClick(e) {
@@ -11,14 +21,29 @@ class BreadCrumb extends React.Component {
   }
 
   render() {
-    let crumbs = this.props.crumbs.map((crumb, index) => {
-      let number = this.props.crumbs.length - index - 1;
-      return <a key={crumb} data-crumb={crumb} data-crumb-number={number}
-                onClick={this.crumbClick}>{crumb} > </a>;
-    });
+    let crumbs = this.props.crumbs;
+    let start = crumbs.indexOf("home");
+
+    if (start !== -1) {
+      crumbs = crumbs.slice(start, crumbs.length);
+      console.log(crumbs);
+      crumbs = crumbs.map((crumb, index) => {
+        let number = crumbs.length - index - 1;
+        let label = crumb;
+        if (this.labels.hasOwnProperty(crumb)) {
+          label = this.labels[crumb];
+        }
+        return <a key={crumb} data-crumb={crumb} data-crumb-number={number}
+                  onClick={this.crumbClick} className="crumb">{label}</a>;
+      });
+
+      crumbs = crumbs.slice(Math.max(0, crumbs.length-3), crumbs.length);
+    } else {
+      crumbs = [];
+    }
     return (
       <nav className="breadcrumbs">
-        {crumbs.splice(Math.max(0, crumbs.length-3), crumbs.length)}
+        {crumbs}
       </nav>
     );
   }
